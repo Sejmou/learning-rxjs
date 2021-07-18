4f04af4ce0dbac9ce382b1c23b4a0ec6a9bc6e77
Sejmou <samo.kolter@gmail.com>
2021-07-18 13:45:23 +0200

    emitting and handling errors in Observable

--- main.js
+++ main.js
@@ -3,28 +3,34 @@ const { Observable } = rxjs;
 
 const simpleObs = new Observable(subscriber => {
     console.log('a simple observable is "executed"');
-    subscriber.next(1);
-    subscriber.next(2);
 
-    setTimeout(() => subscriber.next(3), 1000);
+    let nextVal = 1;
 
-    let nextVal = 4;
     const interval = setInterval(() => {
         console.log('Observable will return new value');
         subscriber.next(nextVal++);
+
+        if (nextVal === 3) subscriber.error('oops, we f*cked up');
     }, 1500);
 
-    // we can return a function that cleans up the Observable after the subscriber unsubscribed 
     return () => {
-        console.log('Subscriber unsubscribed, will not emit any more values');
+        console.log(`Subscriber unsubscribed (or an error occurred), won't emit any more values`);
         clearInterval(interval);
     }
 })
 
-const subscription = simpleObs.subscribe(val => {
-    console.log('got a value from subscription:', val);
+const subWithoutErrHandling = simpleObs.subscribe(val => {
+    console.log('subWithoutErrHandling got a value:', val);
     if (val == 4) {
-        console.log('unsubscribing from subscription');
-        subscription.unsubscribe();// note: this doesn't "kill" the Observable, it still keeps on emitting values...
+        console.log('subWithoutErrHandling unsubscribing from subscription');
+        subWithoutErrHandling.unsubscribe();
     }
 });
+
+const subWithErrHandling = simpleObs.subscribe(val => {
+    console.log('subWithErrHandling got a value from subscription:', val);
+    if (val == 4) {
+        console.log('subWithErrHandling unsubscribing from subscription');
+        subWithErrHandling.unsubscribe();
+    }
+}, err => console.warn(`subWithErrHandling received error ${err}`));
