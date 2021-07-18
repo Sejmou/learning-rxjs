5ea859efd90eeb0cb6aa17354cab85c00d0128f7
Sejmou <samo.kolter@gmail.com>
2021-07-18 23:32:08 +0200

    using map operator included in RxJS

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
-const { Observable, fromEvent } = rxjs;
+const { Observable, fromEvent, operators: { map } } = rxjs;
 
 // Observable -> operator_1 -> ... -> operator_n -> Observable
-
-function map(mappingFn) {
-    return function operator(source) {
-        return new Observable(subscriber => {
-            source.subscribe(val => {
-                subscriber.next(mappingFn(val));
-            });
-        });
-    }
-}
 
 const inputObs = fromEvent(document.getElementById('test-input'), 'input');
 
