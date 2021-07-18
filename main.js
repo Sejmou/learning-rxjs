37f2ec6b895c7f33d55f1e961c8e28a56556df98
Sejmou <samo.kolter@gmail.com>
2021-07-18 23:41:29 +0200

    pipe operator

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const { Observable, fromEvent, operators: { map, filter } } = rxjs;
 
 // Observable -> operator_1 -> ... -> operator_n -> Observable
 
-const filteredTextObs = filter(text => text.length > 3)(
-    map(e => e.target.value)(
-        fromEvent(document.getElementById('test-input'), 'input')
-    ));
+// this is a bit hard to read...
+// const filteredTextObs = filter(text => text.length > 3)(
+//     map(e => e.target.value)(
+//         fromEvent(document.getElementById('test-input'), 'input')
+//     ));
+
+// pipe operator to the rescue!
+const filteredTextObs = fromEvent(document.getElementById('test-input'), 'input').pipe(
+    map(e => e.target.value),
+    filter(text => text.length > 3)
+);
 
 filteredTextObs.subscribe(text => console.log(text));
