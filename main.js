2fa625acbdc6f67fd656c10c0092dbb286a02e9b
Sejmou <samo.kolter@gmail.com>
2021-07-18 14:25:37 +0200

    creating self-made map operator for Observables

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const { Observable, fromEvent } = rxjs;
 
 // Observable -> operator_1 -> ... -> operator_n -> Observable
 
-function inputValOp(source) {// source is an Observable!
-    return new Observable(subscriber => {
-        source.subscribe(val => {
-            subscriber.next(val.target.value);
+function map(mappingFn) {
+    return function operator(source) {
+        return new Observable(subscriber => {
+            source.subscribe(val => {
+                subscriber.next(mappingFn(val));
+            });
         });
-    });
+    }
 }
 
 const inputObs = fromEvent(document.getElementById('test-input'), 'input');
 
-const inputTextObs = inputValOp(inputObs);
+const inputTextObs = map(e => e.target.value)(inputObs);
 
 inputTextObs.subscribe(text => console.log(text));
